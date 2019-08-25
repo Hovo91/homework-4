d316c1099f3f98a51d8f76d02133ee3de875603e
Hovo Mnatsakanyan <51056620+Hovo91@users.noreply.github.com>
2019-08-25 23:15:40 +0400

    Update work-3.js

--- work-3.js
+++ work-3.js
@@ -1,8 +1,8 @@
-function adjacentProductMax(string) {
+function toAcronym (string) {
   let toUpp = string.toUpperCase();
   let arrayOfString = toUpp.split(' ');
   let result = arrayOfString.map((item) => [item[0]]).join('');
   return result;
 }
 
-console.log(adjacentProductMax('Prisoner of War'));
+console.log(toAcronym('Prisoner of War'));
