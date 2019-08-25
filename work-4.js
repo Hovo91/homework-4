1e325144d244d876a5c1e470b6b8f82f6d2c462a
Hovo Mnatsakanyan <51056620+Hovo91@users.noreply.github.com>
2019-08-25 23:59:07 +0400

    Update work-4.js

--- work-4.js
+++ work-4.js
@@ -1,3 +1,5 @@
+// Given a string of digits, output all the contiguous substrings of length n in that string.
+
 function contiguousSubstrings(string, n, result = '') {
   result += string.substring(0, n) + ', ';
   let newString = string.substring(1);
