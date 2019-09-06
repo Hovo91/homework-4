00511efe0575820e6007cd5d38596459a0afd69b
Hovo Mnatsakanyan <51056620+Hovo91@users.noreply.github.com>
2019-09-06 23:17:07 +0400

    Update work-1.js

--- work-1.js
+++ work-1.js
@@ -5,8 +5,7 @@ function adjacentProductMax(array, newArray = []) {
   newArray.push(result.reduce((product, item) => product * item));
   array.shift();
   array.length === 1 ? newArray : adjacentProductMax(array, newArray);
-  newArray.sort((a, b) => a - b);
-  return newArray[newArray.length - 1];
+  return Math.max(...newArray);
 }
 
 console.log(adjacentProductMax([4, 2, 300, 3, 4, 5, 8, 90]));
